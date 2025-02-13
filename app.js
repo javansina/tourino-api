7d995e97c145602cb0b24a7ad2b50be09b41f80c
sina javan <javansina.dev@gmail.com>
2025-02-13 05:32:18 +0330

    fixed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+
 const cors = require("cors");
+
 const swaggerUi = require("swagger-ui-express");
 let swaggerDocument = require("./swagger/swagger.json");
 const path = require("path");
@@ -12,10 +14,36 @@ app.use(express.json());
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-// Set up Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const PORT = process.env.PORT || 6501;
+const startServer = (port) => {
+  const server = app.listen(port, async () => {
+    console.log(`Server running on port ${port}`);
+    swaggerDocument.servers = [
+      {
+        url: `http://localhost:${port}`,
+        description: "Local server",
+      },
+    ];
 
-// Define routes
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    console.log(
+      `Swagger API docs are available at http://localhost:${port}/api-docs`
+    );
+    // Truncate route : http://localhost:<port>/truncate
+  });
+
+  server.on("error", (err) => {
+    if (err.code === "EADDRINUSE") {
+      console.log(`Port ${port} is in use, trying port ${+port + 1}...`);
+      startServer(+port + 1);
+    } else {
+      console.error("Server error:", err);
+    }
+  });
+};
+
+startServer(PORT);
+
 app.use(require("./routes/dev"));
 app.use("/auth", require("./routes/auth"));
 app.use("/tour", require("./routes/tour"));
@@ -26,6 +54,3 @@ app.use("/order", require("./routes/order"));
 app.get("/", (req, res) => {
   res.send("Welcome to the Tour and Travel Agency API!");
 });
-
-// Export app for Vercel
-module.exports = app;
